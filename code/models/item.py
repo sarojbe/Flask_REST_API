2115698f776800124b96478bb903aa204a56ed37
dhansu admi <dhansu@admi.com>
2020-10-31 00:33:35 -0400

    unwated files removed

--- code/models/item.py
+++ code/models/item.py
@@ -21,36 +21,14 @@ class ItemModel(db.Model):
     @classmethod
     def find_by_name(cls,name):
         return cls.query.filter_by(name=name).first()
-        # connection= sqlite3.connect("data.db")
-        # cursor= connection.cursor()
-        # query= "select * from items where name = ?"
-        # result= cursor.execute(query, (name,))
-        # row= result.fetchone()
-        # connection.close()
-        # if row:
-        #     return cls(*row)
-        # return {'message':'Item not found'}, 404
 
 
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-        # connection = sqlite3.connect("data.db")
-        # cursor = connection.cursor()
-        # query = "insert into items values(?,?)"
-        # cursor.execute(query, (self.name, self.price))
-        #
-        # connection.commit()
-        # connection.close()
+
 
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-        # connection = sqlite3.connect("data.db")
-        # cursor = connection.cursor()
-        # query = "update items set price =? where name=?"
-        # cursor.execute(query, ( self.price, self.name))
-        #
-        # connection.commit()
-        # connection.close()
